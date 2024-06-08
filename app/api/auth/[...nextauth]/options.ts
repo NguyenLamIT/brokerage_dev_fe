1a1a67820120fbe4f742d32b9479e04f996c0bed
Do-Manh-Quan <quan.dm194143@sis.hust.edu.vn>
2024-06-08 14:51:22 +0700

    fix google

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -4,7 +4,6 @@ import CredentialsProvider from "next-auth/providers/credentials";
 export const options: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      id: 'Credentials',
       name: "Credentials",
       credentials: {
         type: {
